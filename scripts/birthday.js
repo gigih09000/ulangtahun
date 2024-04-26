868fe2c76091511f339b5ca0ab94f5381724ddce
gigih kusuma wicaksana <70576007+gigih09000@users.noreply.github.com>
2024-04-27 06:37:59 +0700

    Update birthday.js

--- scripts/birthday.js
+++ scripts/birthday.js
@@ -3,7 +3,7 @@
 // Animation GSAP timeline on load event
 window.addEventListener("load", () => {
   Swal.fire({
-    title: "Do you want to play music in the background?",
+    title: "mau pakai musik gaaa?????",
     // text: "You won't be able to revert this!",
     icon: "warning",
     showCancelButton: true,
